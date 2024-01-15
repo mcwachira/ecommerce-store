1b2c8dc76739078c82deceed146b900dafc49c2c
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-01-15 15:26:42 +0300

    Update page.tsx

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -10,39 +10,37 @@ import Summary from './components/Summary'
 export const revalidate = 0;
 
 const CartPage = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    const cart = useCart();
 
-	const [isMounted, setIsMounted] = useState(false)
-	const cart = useCart()
-	console.log(cart.items.length)
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
-	useEffect(() => {
+    if (!isMounted) {
+        return null;
+    }
 
-setIsMounted(true)
-	},[])
-
-	if(!isMounted){
-		return null
-	}
-	  return (
-          <div className="bg-white">
-              <Container>
-                  <div className="px-4 py-16 sm:px-6 lg:px-8">
-                      <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
-                      <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
-                          <div className="lg:col-span-7">
-                              {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
-                              <ul>
-                                  {cart.items.map((item) => (
-                                      <CartItem key={item.id} data={item}/>
-                                  ))}
-                              </ul>
-                          </div>
-                          <Summary/>
-                      </div>
-                  </div>
-              </Container>
-          </div>
-      )
+    return (
+        <div className="bg-white">
+            <Container>
+                <div className="px-4 py-16 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
+                    <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+                        <div className="lg:col-span-7">
+                            {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
+                            <ul>
+                                {cart.items.map((item) => (
+                                    <CartItem key={item.id} data={item} />
+                                ))}
+                            </ul>
+                        </div>
+                        <Summary />
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
 };
 
 export default CartPage;
